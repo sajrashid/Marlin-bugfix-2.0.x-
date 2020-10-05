fbbecb430157dfd7c1a5d6c00dd8696383adef8a
Sajid Rashid <Sajid.Rashid@live.co.uk>
2020-10-05 23:09:51 +0100

    updated bed size to 220

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1099,8 +1099,8 @@
 //@
 
 // The size of the print bed
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 210
+#define X_BED_SIZE 220
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -5
